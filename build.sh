7cadf1f7518621c22787de50103a0afe6733522b
pjoter <>
2022-08-18 10:38:13 +0200

    some tidying up

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-if [-f main.cpp] &&  [-f Int.cpp] && [-f Int.h]
-	g++ -o output.o Int.cpp main.cpp 
-elif
-	echo some files are missing!
+f1=main.cpp
+f2=Int.cpp
+f3=Int.h
+if [ -f "$f1" ] &&  [ -f "$f2" ] && [ -f "$f3" ];
+then g++ -o output.o Int.cpp main.cpp 
+	if [ -f "output.o" ]; then echo compilation comple! 
+	fi
+else echo some files are missing!
 fi
